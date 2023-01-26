20c1f5e77e6aa5e28c7110226fc9ddd4e3ee5931
Taiendy Chyvu <taiendychyvu@gmail.com>
2023-01-26 15:07:13 -0300

    Correção do codigo

--- main.js
+++ main.js
@@ -4,29 +4,24 @@ const formulario = document.getElementById(`formulario`)
 
 formulario.addEventListener(`submit` , function(e){
     e.preventDefault();
-    const valorA = document.getElementById(`valor1`)
-    const valorB = document.getElementById(`valor2`)
-    const valorIntA = parseInt(valorA.value)
-    const valorIntB = parseInt(valorB.value)
-    const resultado = comparar()
+    const valorIntA = parseInt(document.getElementById(`valor1`).value);
+    const valorIntB = parseInt(document.getElementById(`valor2`).value);
+    const resultado = valorIntA < valorIntB
+    const resultadoIgual = valorIntA == valorIntB
     const msgValorAMaior = `<b>${valorIntA}</b> infelizmente é um valor maior que <b>${valorIntB}</b>`
     const msgValorBMaior = `Parabéns o valor <b>${valorIntA}</b> é menor que o <b>${valorIntB}</b>`
+    const msgValorIgual = `O valor <b>${valorIntA}</b> é igual ao valor <b>${valorIntB}</b>`
     const msgResultado = document.querySelector(`.resultado`)
 
-
-    function comparar() {
-        const teste = valorIntA
-        return teste < valorIntB
-    }
-
-    if(resultado) {
+    if (resultadoIgual) {
+        msgResultado.innerHTML = msgValorIgual;
+        msgResultado.style.color = `blue`; 
+    } else if (resultado) {
         msgResultado.innerHTML = msgValorBMaior;
-        msgResultado.style.color = `Green`;
-        
+        msgResultado.style.color = `Green`; 
     } else {
         msgResultado.innerHTML = msgValorAMaior;
         msgResultado.style.color = `red`;
-        
     }
 });
 
